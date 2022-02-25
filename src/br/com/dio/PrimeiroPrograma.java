c9a7da8132d175f4b629ac1da413f3fd63cd2062
cassiocoltri <cassiocoltri@gmail.com>
2022-02-25 12:37:48 -0300

    teste de itelij

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato("Goiaba", "Preta", 3);
+
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 5;
+        System.out.println("Hello bagaça!" + (a+b));*/
+    }
+}
