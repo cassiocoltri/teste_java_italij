9b12a9190f9c91d063b30b701a12ba6d42e23eec
cassiocoltri <cassiocoltri@gmail.com>
2022-02-25 12:52:17 -0300

    update do teste de itelij

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,11 +6,47 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
 
         Gato gato = new Gato("Goiaba", "Preta", 3);
-
         System.out.println(gato);
+
+        Livro livro = new Livro("Juquinha", 400);
+        System.out.println(livro);
 
         /*int a = 5;
         int b = 5;
         System.out.println("Hello bagaça!" + (a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPagina;
+
+    public Livro(String nome, Integer numPagina) {
+        this.nome = nome;
+        this.numPagina = numPagina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPagina() {
+        return numPagina;
+    }
+
+    public void setNumPagina(Integer numPagina) {
+        this.numPagina = numPagina;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPagina=" + numPagina +
+                '}';
     }
 }
